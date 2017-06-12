1407cd4e0f368635977041767d44faaba1030e38
William Rijksen <william@enrise.com>
2017-06-12 17:21:45 +0200

    Initial commit: Introduce module for huawei/samsung devices

---
+++ android/src/ti/optimizationpreferences/TiOptimizationpreferencesModule.java
@@ -0,0 +1,217 @@
+package ti.optimizationpreferences;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Build;
+import android.os.PowerManager;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.appcelerator.kroll.KrollModule;
+import org.appcelerator.kroll.annotations.Kroll;
+
+import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.kroll.common.Log;
+import org.appcelerator.kroll.common.TiConfig;
+
+@Kroll.module(name="TiOptimizationpreferences", id="ti.optimizationpreferences")
+public class TiOptimizationpreferencesModule extends KrollModule {
+    private Intent huawei = intentClassName("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity");
+    private Intent samsung = intentClassName("com.samsung.android.sm", "com.samsung.android.sm.ui.battery.BatteryActivity");
+
+    private Intent[] ALL = new Intent[]{huawei, samsung};
+
+    private final String saveIfSkip = "skipProtectedAppsMessage";
+
+    private static final String LCAT = "TiOptimizationpreferencesModule";
+    private static final boolean DBG = TiConfig.LOGD;
+
+    public TiOptimizationpreferencesModule()
+    {
+        super();
+    }
+
+    @Kroll.onAppCreate
+    public static void onAppCreate(TiApplication app)
+    {
+        Log.d(LCAT, "onAppCreate");
+    }
+
+    @Kroll.method
+    public void check()
+    {
+        final String packageName = getContext().getPackageName();
+        if (shouldSkip()) {
+            return;
+        }
+
+        for (Intent intent : ALL) {
+            if (isCallable(getContext(), intent)) {
+                showWarning(getContext());
+            }
+        }
+    }
+
+    @Kroll.method
+    public boolean needWarning()
+    {
+        for (Intent intent : ALL) {
+            if (isCallable(getContext(), intent)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Intent intentClassName(String packageName, String className)
+    {
+        Intent intent = new Intent();
+        intent.setClassName(packageName, className);
+        return intent;
+    }
+
+    private String getUserSerial(Context context)
+    {
+        Object userManager = context.getSystemService(Context.USER_SERVICE);
+        if (null == userManager)
+            return "";
+        try {
+            Method myUserHandleMethod = android.os.Process.class.getMethod("myUserHandle", (Class<?>[]) null);
+            Object myUserHandle = myUserHandleMethod.invoke(android.os.Process.class, (Object[]) null);
+            Method getSerialNumberForUser = userManager.getClass().getMethod("getSerialNumberForUser", myUserHandle.getClass());
+            Long userSerial = (Long) getSerialNumberForUser.invoke(userManager, myUserHandle);
+            if (userSerial != null) {
+                return String.valueOf(userSerial);
+            } else {
+                return "";
+            }
+        } catch (NoSuchMethodException ignored) {
+        } catch (IllegalArgumentException ignored) {
+        } catch (InvocationTargetException ignored) {
+        } catch (IllegalAccessException ignored) {
+        }
+        return "";
+    }
+
+    private void huaweiProtectedApps(Context context)
+    {
+        try {
+            String cmd = "am start -n " + huawei.getComponent().flattenToShortString();
+            if (Build.VERSION.SDK_INT >= 17) {
+                cmd += " --user " + getUserSerial(context);
+            }
+            Runtime.getRuntime().exec(cmd);
+        } catch (IOException ignored) {
+        }
+    }
+
+    private boolean isCallable(Context context, Intent intent)
+    {
+        List<ResolveInfo> list = context.getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+
+    private AlertDialog.Builder huaweiWarning(Context context)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Huawei");
+        builder.setMessage("Klik op instellingen om te voorkomen dat de app tijdens het fietsen wordt uitgeschakeld. Sta toe dat de app energie-intensieve taken uit mag voeren.");
+        builder.setNeutralButton(android.R.string.no, null);
+        builder.setView(createDontShowAgain());
+        return builder;
+    }
+
+    private AlertDialog.Builder samsungWarning(Context context)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Samsung");
+        builder.setMessage("Overweeg de Samsung SmartManager voor de app uit te zetten om de app in de achtergrond te draaien.");
+        builder.setNeutralButton(android.R.string.no, null);
+        builder.setView(createDontShowAgain());
+        return builder;
+    }
+
+    private AlertDialog.Builder buildWarning(final Context context)
+    {
+        if (isCallable(context, huawei)) {
+            AlertDialog.Builder builder = huaweiWarning(context);
+            builder.setPositiveButton("Instellingen", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    huaweiProtectedApps(context);
+                }
+            });
+            return builder;
+        } else if (isCallable(context, samsung)) {
+            AlertDialog.Builder builder = samsungWarning(context);
+            builder.setPositiveButton("Instellingen", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    context.startActivity(samsung);
+                }
+            });
+            return builder;
+        }
+        return null;
+    }
+
+    private RelativeLayout createDontShowAgain()
+    {
+        final AppCompatCheckBox dontShowAgain = new AppCompatCheckBox(getContext());
+        dontShowAgain.setText("Niet meer tonen");
+        dontShowAgain.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                setSkip(isChecked);
+            }
+        });
+
+        final RelativeLayout layout = new RelativeLayout(getContext());
+        layout.setPadding(30, 30, 0, 0);
+        layout.addView(dontShowAgain);
+
+        return layout;
+    }
+
+    private void showWarning(Context context)
+    {
+        AlertDialog.Builder builder = buildWarning(context);
+        builder.create().show();
+    }
+
+    private SharedPreferences getSettings()
+    {
+        return PreferenceManager.getDefaultSharedPreferences(getContext());
+    }
+
+    private boolean shouldSkip() {
+        return getSettings().getBoolean(saveIfSkip, false);
+    }
+
+    private void setSkip(boolean isChecked)
+    {
+        final SharedPreferences.Editor editor = getSettings().edit();
+        editor.putBoolean(saveIfSkip, isChecked);
+        editor.apply();
+    }
+
+    private Context getContext()
+    {
+        return TiApplication.getInstance().getCurrentActivity();
+    }
+}
---
+++ documentation/index.md
@@ -0,0 +1,39 @@
+# ti.optimizationpreferences Module
+
+## Description
+
+TODO: Enter your module description here
+
+## Accessing the ti.optimizationpreferences Module
+
+To access this module from JavaScript, you would do the following:
+
+    var ti_optimizationpreferences = require("ti.optimizationpreferences");
+
+The ti_optimizationpreferences variable is a reference to the Module object.
+
+## Reference
+
+TODO: If your module has an API, you should document
+the reference here.
+
+### ti_optimizationpreferences.function
+
+TODO: This is an example of a module function.
+
+### ti_optimizationpreferences.property
+
+TODO: This is an example of a module property.
+
+## Usage
+
+TODO: Enter your usage example here
+
+## Author
+
+TODO: Enter your author name, email and other contact
+details you want to share here.
+
+## License
+
+TODO: Enter your license/legal information here.
